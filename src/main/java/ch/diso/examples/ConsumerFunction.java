15dd8187988615d401eb507fec45ddfe72861fdc
Daniel D. Senften <dsenften@diso.ch>
2016-05-23 22:57:01 +0200

    Initial revision.

---
+++ src/main/java/ch/diso/examples/ConsumerFunction.java
@@ -0,0 +1,19 @@
+package ch.diso.examples;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerFunction {
+
+    public static <T> void forEach(List<T> list, Consumer<T> c) {
+        for (T i : list) {
+            c.accept(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 3, 5, 7, 9);
+        forEach(list, System.out::println); // (Integer i) -> System.out.println(i)
+    }
+}
---
+++ src/main/java/ch/diso/examples/PredicateFunction.java
@@ -0,0 +1,27 @@
+package ch.diso.examples;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateFunction {
+
+    public static <T> List<T> filter(List<T> list, Predicate<T> p) {
+        List<T> result = new ArrayList<>();
+        for (T s : list) {
+            if (p.test(s)) {
+                result.add(s);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<String> list = filter(
+                Arrays.asList("Daniel", "", "Senften"),
+                (String s) -> !s.isEmpty());
+        System.out.println(list);
+    }
+
+}
